87312b098cf0af87ff5e7070ddbfc315f0cacf32
pskjksr <friendzazaza555@gmail.com>
2025-01-13 22:46:00 +0700

    ลิงก์ทำงานเฉพาะปุ่ม "RECIPE"

--- app/Categories/page.jsx
+++ app/Categories/page.jsx
@@ -13,144 +13,147 @@ export default function Breakfast() {
   return (
     <section className="recipe-section">
             <div className="recipe-category">
-              <h2>Breakfast</h2>
-              <div className="recipe-grid">
-            {/* <!-- Card 1 --> */}
-            {Breakfast.map((item) => (
-              <Link href={`/RecipeDescription/${item.slug}`} key={item.id}>
-                <div className="recipe-card">
-                  <div className="flex justify-center">
-                    <img
-                      src={`/${item.image}`}
-                      alt={item.title}
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
-                  <div className="card-content">
-                    <h3>{item.title}</h3>
-                    <button>RECIPE</button>
-                  </div>
-                </div>
-              </Link>
-            ))}
+    <h2>Breakfast</h2>
+    <div className="recipe-grid">
+      {/* <!-- Card 1 --> */}
+      {Breakfast.map((item) => (
+        <div className="recipe-card" key={item.id}>
+          <div className="flex justify-center">
+            <img
+              src={`/${item.image}`}
+              alt={item.title}
+              style={{
+                width: "150px",
+                height: "150px",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="card-content">
+            <h3>{item.title}</h3>
+            <Link href={`/RecipeDescription/${item.slug}`}>
+              <button>RECIPE</button>
+            </Link>
           </div>
         </div>
 
-        <div className="recipe-category">
-              <h2>Lunch</h2>
-              <div className="recipe-grid">
-            {/* <!-- Card 2 --> */}
-            {Lunch.map((item) => (
-              <Link href={`/RecipeDescription/${item.slug}`} key={item.id}>
-                <div className="recipe-card">
-                  <div className="flex justify-center">
-                    <img
-                      src={`/${item.image}`}
-                      alt={item.title}
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
-                  <div className="card-content">
-                    <h3>{item.title}</h3>
-                    <button>RECIPE</button>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+      ))}
+    </div>
+  </div>
+
+  <div className="recipe-category">
+  <h2>Lunch</h2>
+  <div className="recipe-grid">
+    {/* <!-- Card 2 --> */}
+    {Lunch.map((item) => (
+      <div className="recipe-card" key={item.id}>
+        <div className="flex justify-center">
+          <img
+            src={`/${item.image}`}
+            alt={item.title}
+            style={{
+              width: "150px",
+              height: "150px",
+              objectFit: "cover",
+            }}
+          />
         </div>
+        <div className="card-content">
+          <h3>{item.title}</h3>
+          <Link href={`/RecipeDescription/${item.slug}`}>
+            <button>RECIPE</button>
+          </Link>
+        </div>
+      </div>
+
+    ))}
+  </div>
+</div>
+
 
         <div className="recipe-category">
-              <h2>Dinner</h2>
-              <div className="recipe-grid">
-            {/* <!-- Card 3 --> */}
-            {Dinner.map((item) => (
-              <Link href={`/RecipeDescription/${item.slug}`} key={item.id}>
-                <div className="recipe-card">
-                  <div className="flex justify-center">
-                    <img
-                      src={`/${item.image}`}
-                      alt={item.title}
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
-                  <div className="card-content">
-                    <h3>{item.title}</h3>
-                    <button>RECIPE</button>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+  <h2>Dinner</h2>
+  <div className="recipe-grid">
+    {/* <!-- Card 3 --> */}
+    {Dinner.map((item) => (
+      <div className="recipe-card" key={item.id}>
+        <div className="flex justify-center">
+          <img
+            src={`/${item.image}`}
+            alt={item.title}
+            style={{
+              width: "150px",
+              height: "150px",
+              objectFit: "cover",
+            }}
+          />
         </div>
+        <div className="card-content">
+          <h3>{item.title}</h3>
+          <Link href={`/RecipeDescription/${item.slug}`}>
+            <button>RECIPE</button>
+          </Link>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
 
-        <div className="recipe-category">
-              <h2>Dessert</h2>
-              <div className="recipe-grid">
-            {/* <!-- Card 4 --> */}
-            {Dessert.map((item) => (
-              <Link href={`/RecipeDescription/${item.slug}`} key={item.id}>
-                <div className="recipe-card">
-                  <div className="flex justify-center">
-                    <img
-                      src={`/${item.image}`}
-                      alt={item.title}
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
-                  <div className="card-content">
-                    <h3>{item.title}</h3>
-                    <button>RECIPE</button>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+<div className="recipe-category">
+  <h2>Dessert</h2>
+  <div className="recipe-grid">
+    {/* <!-- Card 4 --> */}
+    {Dessert.map((item) => (
+      <div className="recipe-card" key={item.id}>
+        <div className="flex justify-center">
+          <img
+            src={`/${item.image}`}
+            alt={item.title}
+            style={{
+              width: "150px",
+              height: "150px",
+              objectFit: "cover",
+            }}
+          />
         </div>
+        <div className="card-content">
+          <h3>{item.title}</h3>
+          <Link href={`/RecipeDescription/${item.slug}`}>
+            <button>RECIPE</button>
+          </Link>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
 
-        <div className="recipe-category">
-              <h2>Drink</h2>
-              <div className="recipe-grid">
-            {/* <!-- Card 5 --> */}
-            {Drink.map((item) => (
-              <Link href={`/RecipeDescription/${item.slug}`} key={item.id}>
-                <div className="recipe-card">
-                  <div className="flex justify-center">
-                    <img
-                      src={`/${item.image}`}
-                      alt={item.title}
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
-                  <div className="card-content">
-                    <h3>{item.title}</h3>
-                    <button>RECIPE</button>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+<div className="recipe-category">
+  <h2>Drink</h2>
+  <div className="recipe-grid">
+    {/* <!-- Card 5 --> */}
+    {Drink.map((item) => (
+      <div className="recipe-card" key={item.id}>
+        <div className="flex justify-center">
+          <img
+            src={`/${item.image}`}
+            alt={item.title}
+            style={{
+              width: "150px",
+              height: "150px",
+              objectFit: "cover",
+            }}
+          />
         </div>
+        <div className="card-content">
+          <h3>{item.title}</h3>
+          <Link href={`/RecipeDescription/${item.slug}`}>
+            <button>RECIPE</button>
+          </Link>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
 
     </section>
   );
