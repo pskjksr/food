414884cb2469692baa340632805518252bb03fe5
pskjksr <friendzazaza555@gmail.com>
2025-01-14 16:28:16 +0700

    เปลี่ยนจุด เป็นสีเหลือง

--- app/Like/page.jsx
+++ app/Like/page.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import "./Like.css";
+import data from "../data";
+import Link from "next/link";
 
 function Like() {
+
+    const Breakfast = data.filter((item) => item.category === "Breakfast");
+
     return (
-        <div className="container">
-            <span className="like-label">&#x2b50; Like</span>
-            <div className="grid">
-                {Array(4).fill(null).map((_, index) => (
-                    <div className="card" key={index}>
-                        <img
-                            src="https://via.placeholder.com/300"
-                            alt="Ramen"
-                        />
-                        <h3>Ramen: ラーメン</h3>
-                        <button className="recipe-button">Recipe</button>
-                    </div>
-                ))}
-            </div>
+        <section className="recipe-section">
+        <div className="recipe-category">
+    <h2>
+      <span style={{ color: "#EFBD4C" }}>●</span> like
+    </h2>
+    <div className="recipe-grid">
+      {/* <!-- Card 1 --> */}
+      {Breakfast.map((item) => (
+        <div className="recipe-card" key={item.id}>
+          <div className="flex justify-center">
+            <img
+              src={`/${item.image}`}
+              alt={item.title}
+              style={{
+                width: "150px",
+                height: "150px",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="card-content">
+            <h3>{item.title}</h3>
+            <Link href={`/RecipeDescription/${item.slug}`}>
+              <button>RECIPE</button>
+            </Link>
+          </div>
         </div>
+
+      ))}
+    </div>
+  </div>
+  </section>
     );
 }
 
