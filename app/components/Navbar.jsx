ddbb4093eaca7029bee7541e0409cacb854f6dbd
Phonsinee <66022804@up.ac.th>
2025-01-31 13:24:34 +0700

    Adjust spacing, change font size

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -18,31 +18,31 @@ function Navbar() {
   };
 
   return (
-    <div className="flex justify-between items-center relative font-semibold text-xl px-5 py-2">
-      <div><Image src="/logo.png" width={50} height={50} alt="logo" /></div>
+    <div className="flex justify-between items-center  font-semibold text-xl px-5 py-2 ">
+      <div className='z-50'><Image src="/logo.png" width={50} height={50} alt="logo" /></div>
       <nav className="ml-40">
-        <ul className="flex gap-5">
+        <ul className="flex gap-20">
           <li>
-            <a href="/homepage">HOME</a>
+            <a className='text-[25px]' href="/homepage">HOME</a>
           </li>
           <li>
-            <a className="cursor-pointer" onClick={scrollToSection}>
+            <a className="cursor-pointer text-[25px]" onClick={scrollToSection}>
               RECIPE
             </a>
           </li>
           <li>
-            <Link href="/Categories">CATEGORIES</Link>
+            <Link className='text-[25px]' href="/Categories">CATEGORIES</Link>
           </li>
         </ul>
       </nav>
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-12">
         <div className="bg-yellow-500 flex rounded-full p-1">
           <input
             type="text"
             className="bg-transparent rounded-full px-4 outline-yellow-500"
           />
           <div className="p-1">
-            <i className="fas fa-search text-white"></i>
+            <i className="fas fa-search pr-2 text-white"></i>
           </div>
         </div>
         <div className="relative">
