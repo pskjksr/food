4c67119f5154f9b964a7b256edfbf8d6cadcec76
Phonsinee <66022804@up.ac.th>
2025-01-31 13:20:41 +0700

    Change color, adjust font size ,padding,margin

--- app/Categories/page.jsx
+++ app/Categories/page.jsx
@@ -13,10 +13,10 @@ export default function Breakfast() {
   return (
     <section className="recipe-section">
       <div className="recipe-category">
-        <div className="ml-5 font-semibold text-lg text-gray-700">
+        <div className="ml-5 mt-5 font-semibold text-[26px] text-gray-700">
           <h2>Breakfast</h2>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 p-5">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-16 p-5">
           {/* <!-- Card 1 --> */}
           {Breakfast.map((item) => (
             <div
@@ -24,7 +24,7 @@ export default function Breakfast() {
               key={item.id}
             >
               {/* รูปภาพ */}
-              <div className="flex justify-center">
+              <div className="flex justify-center mt-5">
                 <img
                   src={`/${item.image}`}
                   alt={item.title}
@@ -36,28 +36,28 @@ export default function Breakfast() {
                 />
               </div>
               {/* เนื้อหาในการ์ด */}
-              <div className="relative p-4 text-center">
-                <h3 className="mb-4">{item.title}</h3>
+              <div className="relative p-10 text-center">
+                <h3 className="mb-14 text-[20px]">{item.title}</h3>
                 {/* ปุ่ม RECIPE อยู่ในลิงก์ */}
                 <Link href={`/RecipeDescription/${item.slug}`}>
-                  <button className="absolute right-4 bottom-1 px-4 py-2 bg-yellow-400 text-gray-800 text-sm rounded-md hover:bg-yellow-500 transition-colors duration-300">
+                  <button className="absolute right-5 bottom-5 px-4 py-2 border-2 bg-yellow-400 text-gray-800 text-md rounded-xl border-[#FFECC1] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] transition-colors duration-300">
                     RECIPE
                   </button>
                 </Link>
               </div>
 
               {/* หัวใจในตำแหน่งที่ถูกต้อง */}
-              <i className="fa-solid fa-heart absolute bottom-2 left-2 border-2 text-sm border-[#FFECC1] bg-[#EFBD4C] text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-1.5 cursor-pointer"></i>
+              <i className="fa-solid fa-heart absolute bottom-5 left-5 border-2 text-xl border-[#FFECC1] bg-yellow-400 text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-2 cursor-pointer"></i>
             </div>
           ))}
         </div>
       </div>
 
       <div className="recipe-category">
-        <div className="ml-5 font-semibold text-lg text-gray-700">
+        <div className="ml-5 font-semibold text-[26px] text-gray-700">
           <h2>Lunch</h2>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 p-5">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-16 p-5">
           {/* <!-- Card 2 --> */}
           {Lunch.map((item) => (
             <div
@@ -65,7 +65,7 @@ export default function Breakfast() {
               key={item.id}
             >
               {/* รูปภาพ */}
-              <div className="flex justify-center">
+              <div className="flex justify-center mt-5">
                 <img
                   src={`/${item.image}`}
                   alt={item.title}
@@ -77,28 +77,28 @@ export default function Breakfast() {
                 />
               </div>
               {/* เนื้อหาในการ์ด */}
-              <div className="relative p-4 text-center">
-                <h3 className="mb-4">{item.title}</h3>
+              <div className="relative p-10 text-center">
+                <h3 className="mb-14 text-[20px]">{item.title}</h3>
                 {/* ปุ่ม RECIPE อยู่ในลิงก์ */}
                 <Link href={`/RecipeDescription/${item.slug}`}>
-                  <button className="absolute right-4 bottom-1 px-4 py-2 bg-yellow-400 text-gray-800 text-sm rounded-md hover:bg-yellow-500 transition-colors duration-300">
+                  <button className="absolute right-5 bottom-5 px-4 py-2 border-2 bg-yellow-400 text-gray-800 text-md rounded-xl border-[#FFECC1] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] transition-colors duration-300">
                     RECIPE
                   </button>
                 </Link>
               </div>
 
               {/* หัวใจในตำแหน่งที่ถูกต้อง */}
-              <i className="fa-solid fa-heart absolute bottom-2 left-2 border-2 text-sm border-[#FFECC1] bg-[#EFBD4C] text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-1.5 cursor-pointer"></i>
+              <i className="fa-solid fa-heart absolute bottom-5 left-5 border-2 text-xl border-[#FFECC1] bg-yellow-400 text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-2 cursor-pointer"></i>
             </div>
           ))}
         </div>
       </div>
 
       <div className="recipe-category">
-        <div className="ml-5 font-semibold text-lg text-gray-700">
+        <div className="ml-5 font-semibold text-[26px] text-gray-700">
           <h2>Dinner</h2>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 p-5">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-16 p-5">
           {/* <!-- Card 3 --> */}
           {Dinner.map((item) => (
             <div
@@ -106,7 +106,7 @@ export default function Breakfast() {
               key={item.id}
             >
               {/* รูปภาพ */}
-              <div className="flex justify-center">
+              <div className="flex justify-center mt-5">
                 <img
                   src={`/${item.image}`}
                   alt={item.title}
@@ -118,28 +118,28 @@ export default function Breakfast() {
                 />
               </div>
               {/* เนื้อหาในการ์ด */}
-              <div className="relative p-4 text-center">
-                <h3 className="mb-4">{item.title}</h3>
+              <div className="relative p-10 text-center">
+                <h3 className="mb-14 text-[20px]">{item.title}</h3>
                 {/* ปุ่ม RECIPE อยู่ในลิงก์ */}
                 <Link href={`/RecipeDescription/${item.slug}`}>
-                  <button className="absolute right-4 bottom-1 px-4 py-2 bg-yellow-400 text-gray-800 text-sm rounded-md hover:bg-yellow-500 transition-colors duration-300">
+                  <button className="absolute right-5 bottom-5 px-4 py-2 border-2 bg-yellow-400 text-gray-800 text-md rounded-xl border-[#FFECC1] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] transition-colors duration-300">
                     RECIPE
                   </button>
                 </Link>
               </div>
 
               {/* หัวใจในตำแหน่งที่ถูกต้อง */}
-              <i className="fa-solid fa-heart absolute bottom-2 left-2 border-2 text-sm border-[#FFECC1] bg-[#EFBD4C] text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-1.5 cursor-pointer"></i>
+              <i className="fa-solid fa-heart absolute bottom-5 left-5 border-2 text-xl border-[#FFECC1] bg-yellow-400 text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-2 cursor-pointer"></i>
             </div>
           ))}
         </div>
       </div>
 
       <div className="recipe-category">
-        <div className="ml-5 font-semibold text-lg text-gray-700">
+        <div className="ml-5 font-semibold text-[26px] text-gray-700">
           <h2>Dessert</h2>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 p-5">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-16 p-5">
           {/* <!-- Card 4 --> */}
           {Dessert.map((item) => (
             <div
@@ -147,7 +147,7 @@ export default function Breakfast() {
               key={item.id}
             >
               {/* รูปภาพ */}
-              <div className="flex justify-center">
+              <div className="flex justify-center mt-5">
                 <img
                   src={`/${item.image}`}
                   alt={item.title}
@@ -159,28 +159,28 @@ export default function Breakfast() {
                 />
               </div>
               {/* เนื้อหาในการ์ด */}
-              <div className="relative p-4 text-center">
-                <h3 className="mb-4">{item.title}</h3>
+              <div className="relative p-10 text-center">
+                <h3 className="mb-14 text-[20px]">{item.title}</h3>
                 {/* ปุ่ม RECIPE อยู่ในลิงก์ */}
                 <Link href={`/RecipeDescription/${item.slug}`}>
-                  <button className="absolute right-4 bottom-1 px-4 py-2 bg-yellow-400 text-gray-800 text-sm rounded-md hover:bg-yellow-500 transition-colors duration-300">
+                  <button className="absolute right-5 bottom-5 px-4 py-2 border-2 bg-yellow-400 text-gray-800 text-md rounded-xl border-[#FFECC1] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] transition-colors duration-300">
                     RECIPE
                   </button>
                 </Link>
               </div>
 
               {/* หัวใจในตำแหน่งที่ถูกต้อง */}
-              <i className="fa-solid fa-heart absolute bottom-2 left-2 border-2 text-sm border-[#FFECC1] bg-[#EFBD4C] text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-1.5 cursor-pointer"></i>
+              <i className="fa-solid fa-heart absolute bottom-5 left-5 border-2 text-xl border-[#FFECC1] bg-yellow-400 text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-2 cursor-pointer"></i>
             </div>
           ))}
         </div>
       </div>
 
       <div className="recipe-category">
-        <div className="ml-5 font-semibold text-lg text-gray-700">
+        <div className="ml-5 font-semibold text-[26px] text-gray-700">
           <h2>Drink</h2>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 p-5">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-16 p-5">
           {/* <!-- Card 5 --> */}
           {Drink.map((item) => (
             <div
@@ -188,7 +188,7 @@ export default function Breakfast() {
               key={item.id}
             >
               {/* รูปภาพ */}
-              <div className="flex justify-center">
+              <div className="flex justify-center mt-5">
                 <img
                   src={`/${item.image}`}
                   alt={item.title}
@@ -200,18 +200,18 @@ export default function Breakfast() {
                 />
               </div>
               {/* เนื้อหาในการ์ด */}
-              <div className="relative p-4 text-center">
-                <h3 className="mb-4">{item.title}</h3>
+              <div className="relative p-10 text-center">
+                <h3 className="mb-14 text-[18px]">{item.title}</h3>
                 {/* ปุ่ม RECIPE อยู่ในลิงก์ */}
                 <Link href={`/RecipeDescription/${item.slug}`}>
-                  <button className="absolute right-4 bottom-1 px-4 py-2 bg-yellow-400 text-gray-800 text-sm rounded-md hover:bg-yellow-500 transition-colors duration-300">
+                  <button className="absolute right-5 bottom-5 px-4 py-2 border-2 bg-yellow-400 text-gray-800 text-md rounded-xl border-[#FFECC1] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] transition-colors duration-300">
                     RECIPE
                   </button>
                 </Link>
               </div>
 
               {/* หัวใจในตำแหน่งที่ถูกต้อง */}
-              <i className="fa-solid fa-heart absolute bottom-2 left-2 border-2 text-sm border-[#FFECC1] bg-[#EFBD4C] text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-1.5 cursor-pointer"></i>
+              <i className="fa-solid fa-heart absolute bottom-5 left-5 border-2 text-xl border-[#FFECC1] bg-yellow-400 text-[#fbfbfb] hover:text-[#FFB100] hover:bg-[#FFECC1] hover:border-[#EFBD4C] active:bg-[#F8F8F8] rounded-full p-2 cursor-pointer"></i>
             </div>
           ))}
         </div>
